b76cb6e9f4e02f5ae33e79cb6a73d9b1f2d72571
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-23 11:20:57 +0530

    Update GetColor.hpp

--- GetColor.hpp
+++ GetColor.hpp
@@ -5,14 +5,6 @@ namespace TelCoColorCoder
     enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
     enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
 
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
